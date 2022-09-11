4171f7bca0ba8124a0c7e2f2d0ac319b9055c018
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-09-12 00:02:08 +0300

    add firebase

--- src/pages/LoginForm/LoginForm.js
+++ src/pages/LoginForm/LoginForm.js
@@ -5,11 +5,19 @@ import './LoginFormStyle.css';
 
 const LoginForm = () => {
 
+    const submit = (e) => {
+        e.preventDefault()
+
+
+        e.target.email.value = '';
+        e.target.password.value = '';
+    }
+
     return (
         <div className={'loginForm'}>
-            <form className={'form'}>
-                <input type="text" placeholder={'email'}/>
-                <input type="password" placeholder={'password'}/>
+            <form className={'form'} onSubmit={submit}>
+                <input type="text" placeholder={'email'} name={'email'}/>
+                <input type="password" placeholder={'password'} name={'password'}/>
                 <button>add</button>
             </form>
         </div>
